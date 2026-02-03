f7a5ad73e6edd7b86236060cba7369e09e26963a
Sachin Singh <23BCS12130@cuchd.in>
2026-02-03 10:09:21 +0530

    Update trustedOrigins to use allowedOrigins array

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,13 +4,17 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 const isProduction = env.NODE_ENV === "production";
-
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://news-monkey-six-rho.vercel.app",
+];
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
 
-  trustedOrigins: env.CORS_ORIGIN,
+  trustedOrigins: allowedOrigins,
   emailAndPassword: {
     enabled: true,
   },
